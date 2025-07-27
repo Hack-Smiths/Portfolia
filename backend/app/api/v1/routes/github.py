4a109fcf51dafbab5defa0e6b242a3d43b83d7b6
Darkknight1213 <riyazahamedff@gmail.com>
2025-07-28 02:41:08 +0530

    backend

--- backend/app/api/v1/routes/github.py
+++ backend/app/api/v1/routes/github.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter, Query
+from fastapi import APIRouter
 from urllib.parse import urlparse
 import requests, os
 from dotenv import load_dotenv
+import base64
 
 load_dotenv()
 router = APIRouter()
@@ -14,23 +15,32 @@ headers = {
 }
 
 @router.get("/repo-info")
-def get_repo_info(repo_url: str = Query(...)):
+def get_repo_info():
     try:
+        # Hardcoded repo URL for testing
+        repo_url = "https://github.com/YBIFoundation/Internship"
         path = urlparse(repo_url).path.strip("/")
-        owner, repo = path.split("/")[:2]
+        owner, repo = path.replace(".git", "").split("/")[:2]
 
+        # GitHub repo API
         repo_resp = requests.get(
-            f"https://api.github.com/repos/{owner}/{repo}",
+            f"https://api.github.com/repos/YBIFoundation/Internship",
             headers=headers
         )
         repo_data = repo_resp.json()
 
+        # GitHub readme API
         readme_resp = requests.get(
-            f"https://api.github.com/repos/{owner}/{repo}/readme",
+            f"https://api.github.com/repos//YBIFoundation/Internship",
             headers=headers
         )
         readme_data = readme_resp.json()
-        readme_content = readme_data.get("content")
+        readme_content_base64 = readme_data.get("content")
+
+        readme_decoded = None
+        if readme_content_base64:
+            readme_decoded = base64.b64decode(readme_content_base64).decode("utf-8")
+
 
         return {
             "name": repo_data.get("name"),
@@ -39,7 +49,8 @@ def get_repo_info(repo_url: str = Query(...)):
             "forks": repo_data.get("forks_count"),
             "description": repo_data.get("description"),
             "html_url": repo_data.get("html_url"),
-            "readme_content_base64": readme_content
+            "readme_content_base64": readme_decoded
         }
+
     except Exception as e:
         return {"error": str(e)}
