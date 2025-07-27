a9b2208561864bb2cfd3864fab182c9a37a8068b
Darkknight1213 <riyazahamedff@gmail.com>
2025-07-28 02:50:15 +0530

    Added getting functionality for repo link

--- backend/app/api/v1/routes/github.py
+++ backend/app/api/v1/routes/github.py
@@ -1,8 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from urllib.parse import urlparse
-import requests, os
+import requests, os, base64
 from dotenv import load_dotenv
-import base64
 
 load_dotenv()
 router = APIRouter()
@@ -15,32 +14,29 @@ headers = {
 }
 
 @router.get("/repo-info")
-def get_repo_info():
+def get_repo_info(repo_url: str = Query(..., description="Full GitHub repo URL")):
     try:
-        # Hardcoded repo URL for testing
-        repo_url = "https://github.com/YBIFoundation/Internship"
-        path = urlparse(repo_url).path.strip("/")
+        parsed = urlparse(repo_url)
+        path = parsed.path.strip("/")
         owner, repo = path.replace(".git", "").split("/")[:2]
 
-        # GitHub repo API
         repo_resp = requests.get(
-            f"https://api.github.com/repos/YBIFoundation/Internship",
+            f"https://api.github.com/repos/{owner}/{repo}",
             headers=headers
         )
         repo_data = repo_resp.json()
 
-        # GitHub readme API
         readme_resp = requests.get(
-            f"https://api.github.com/repos//YBIFoundation/Internship",
+            f"https://api.github.com/repos/{owner}/{repo}/readme",
             headers=headers
         )
         readme_data = readme_resp.json()
-        readme_content_base64 = readme_data.get("content")
 
+        readme_content_base64 = readme_data.get("content")
         readme_decoded = None
+
         if readme_content_base64:
             readme_decoded = base64.b64decode(readme_content_base64).decode("utf-8")
-
 
         return {
             "name": repo_data.get("name"),
@@ -49,7 +45,7 @@ def get_repo_info():
             "forks": repo_data.get("forks_count"),
             "description": repo_data.get("description"),
             "html_url": repo_data.get("html_url"),
-            "readme_content_base64": readme_decoded
+            "readme": readme_decoded
         }
 
     except Exception as e:
