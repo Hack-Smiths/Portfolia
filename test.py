97685096b80d2c0e949b42c276b6cdfe43751a91
Mohamed Riyaz Ahamed <91612487+Darkknight1213@users.noreply.github.com>
2025-07-30 01:14:08 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from urllib.parse import urlparse
-parsed = urlparse("https://github.com/Tech-Smiths/Voix-It")
-path = parsed.path.strip("/")
-owner, repo = path.replace(".git", "").split("/")[:2]
-print(parsed)
-print(path)
-print(owner)
-print(repo)
